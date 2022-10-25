ea9d1e0435d6fa056b79c78aa7d72e6a54956ead
w67652 <maxspore4444@gmail.com>
2022-10-25 16:54:25 +0200

    lab 3

---
+++ lab3.2.py
@@ -0,0 +1,5 @@
+#zadanie 2
+x=float(-4)
+while x<5:
+    print("f(",x,")", 2*x**2-5*x-8)
+    x+=0.5
---
+++ lab3.3.py
@@ -0,0 +1,6 @@
+#zadanie 3
+x=1
+while True:
+    x=int(input("Podaj liczbę "))
+    if x<0:
+        break
