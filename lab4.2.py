a435d8d896441113f779d4f6fea953f7d8a369c5
w67652 <maxspore4444@gmail.com>
2022-11-08 16:58:44 +0100

    lab 4

--- lab4.2.py
+++ lab4.2.py
@@ -6,3 +6,9 @@ for i in range (x):
     L1.append(random.randrange(1,11))
     x-=1
 print(L1)
+y= int(input("Podaj długość listy "))
+L2=[]
+for j in range (y):
+    L2.append(random.randrange(5,16))
+    y-=1
+print(L2)
