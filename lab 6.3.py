a7f7f70d4fe9c2560b51a77fe73dbd55cab9644a
w67652 <maxspore4444@gmail.com>
2022-11-29 16:55:20 +0100

    lab 6

--- lab 6.3.py
+++ lab 6.3.py
@@ -5,4 +5,15 @@ def job(*na):
 
 job(1,60)
 
-job(8,"jo","nom")
+job(8,300,19)
+
+def job(*na):
+    for i in na:
+        print(i)
+def max(num1,*args):
+    m = num1
+    for i in args:
+        if m < i:
+            m = 1
+    return m
+print(max(8,-4,5,7))
